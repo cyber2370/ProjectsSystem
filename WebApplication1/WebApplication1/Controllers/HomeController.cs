d956696987165316a3dfd9e94ff3c44fe9694405
user <user@DESKTOP-FUU8Q9O>
2016-10-31 18:42:12 +0200

    Updated packages, added react page sample, connected React lib.

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -2,16 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
-
+using System.Web.UI;
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
-
             return View();
         }
     }
