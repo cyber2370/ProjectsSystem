4be5feab6cb29eae8c60f209c88037467db084ba
user <user@DESKTOP-FUU8Q9O>
2016-11-09 11:16:27 +0200

    Fixed connection string

--- WebApplication1/DatabaseStorage/Data/ApplicationDbContext.cs
+++ WebApplication1/DatabaseStorage/Data/ApplicationDbContext.cs
@@ -1,11 +1,14 @@
 ﻿using System.Data.Entity;
+using System.Data.SqlClient;
 using DatabaseStorage.Entities;
 
 namespace DatabaseStorage.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext() : base("name=DbContext")
+        private const string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog = ProjectsSystemDb; Integrated Security = True; Connect Timeout = 30; Encrypt=False; TrustServerCertificate=True; ApplicationIntent=ReadWrite; MultiSubnetFailover=False";
+
+        public ApplicationDbContext() : base(ConnectionString)
         {
 
         }
